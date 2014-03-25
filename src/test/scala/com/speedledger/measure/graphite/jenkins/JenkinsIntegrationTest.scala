ffc226818d6750b34fb783b6a3f10d24d92ae6fb
Marcus Lönnberg <ml@speedledger.se>
2014-03-25 11:03:42 +0100

    Add more tests in JenkinsIntegrationTest

--- src/test/scala/com/speedledger/measure/graphite/jenkins/JenkinsIntegrationTest.scala
+++ src/test/scala/com/speedledger/measure/graphite/jenkins/JenkinsIntegrationTest.scala
@@ -1,6 +1,6 @@
 package com.speedledger.measure.graphite.jenkins
 
-import org.scalatest.{Matchers, FunSuite}
+import org.scalatest.{FreeSpec, Matchers, FunSuite}
 import org.scalatest.concurrent.ScalaFutures
 import akka.actor.{Props, ActorSystem}
 import akka.util.Timeout
@@ -10,30 +10,44 @@ import com.speedledger.measure.graphite.{ElasticsearchActorTest, Update, Graphit
 import scala.concurrent.Promise
 import spray.http.HttpRequest
 import org.joda.time.DateTime
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import com.typesafe.config.{Config, ConfigFactory}
 
 /**
  * Integration test on [[com.speedledger.measure.graphite.jenkins.JenkinsActor]] together with 
  * [[com.speedledger.measure.graphite.ElasticsearchActor]] and [[com.speedledger.measure.graphite.GraphiteActor]].
  * Mocks interaction with remote services.
  */
-class JenkinsIntegrationTest extends FunSuite with Matchers with ScalaFutures {
+class JenkinsIntegrationTest extends FreeSpec with Matchers with ScalaFutures {
   implicit val system = ActorSystem("jenkins-integration-test")
 
   implicit val timeout = Timeout(1.second)
   implicit val patience = PatienceConfig(timeout = 1.second)
 
-  test("On update Jenkins grabs data from Elasticsearch and sends to Graphite") {
-    TestActorRef(Props(new ElasticsearchActorMock(Promise[HttpRequest](), ElasticsearchActorTest.httpResponseWithTestData)), "elasticsearch")
+  val jenkinsConfig = ConfigFactory.parseString("""|query-time-adjustment = 1 milliseconds
+                                                   |query-size = 100
+                                                   |url = "http://graphite"""".stripMargin)
+
+  "On update Jenkins should fetch data from Elasticsearch and sends measures to Graphite" - {
+    val elasticRequest = Promise[HttpRequest]()
+    TestActorRef(Props(new ElasticsearchActorMock(elasticRequest, ElasticsearchActorTest.httpResponseWithTestData)), "elasticsearch")
 
     val graphiteRequest = Promise[HttpRequest]()
     TestActorRef(Props(new GraphiteActorMock(graphiteRequest)), "graphite")
 
-    val jenkins = TestActorRef(Props[JenkinsActor])
+    val jenkins = TestActorRef(Props(new JenkinsActorMock(jenkinsConfig)))
 
     val queryTime = DateTime.now
     jenkins ! Update(queryTime)
 
-    graphiteRequest.future.futureValue.entity.asString should be ===
+    // The query sent to ES should use the query time.
+    val json = parse(elasticRequest.future.futureValue.entity.asString)
+    (json \\ "_timestamp").children.head shouldEqual JInt(queryTime.getMillis - 1)
+
+    graphiteRequest.future.futureValue.entity.asString shouldEqual
       "jenkins.builds.Awesome_project.success.duration 26984 1392731047"
   }
 }
+
+class JenkinsActorMock(override val config: Config) extends JenkinsActor
